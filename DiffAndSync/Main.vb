23cd1cd16fe4c2a397a87b376a4a3032a5f8ddea
kmdkmd <kmd217@gmail.com>
2013-11-17 04:23:56 +0900

    実行前確認処理実装

--- DiffAndSync/Main.vb
+++ DiffAndSync/Main.vb
@@ -4,46 +4,154 @@ Public Class Main
 
 #Region "定数"
 
-    Private Const LOG_FILE_FORMAT = """diffandsync_""yyyyMMddHHmmss"".log"""
+    ' 開始前確認メッセージ
+    Private Const DIFF_CONFIRM_TITLE As String = "差分取得開始確認"
+    Private Const DIFF_CONFIRM_MSG As String = "差分取得してもいいですか？"
+    Private Const SYNC_CONFIRM_TITLE As String = "同期開始確認"
+    Private Const SYNC_CONFIRM_MSG As String = "同期してもいいですか？"
+
+    ' 共通メッセージ
+    Private Const CHECK_ERROR_TITLE As String = "エラー"
+    Private Const PATH_IS_EMPTY As String = """{0}"" が空です。"
+    Private Const IS_NOT_FOLDER_MSG As String = """{0}"" は" & vbCrLf & "フォルダではなくファイルです。フォルダを指定して下さい。"
+    Private Const IS_NOT_FILE_MSG As String = """{0}"" は" & vbCrLf & "ファイルではフォルダではなくです。ファイルを指定して下さい。"
+    Private Const IS_NOT_EXISTS_MSG As String = """{0}"" は" & vbCrLf & "存在しません。"
+    Private Const FILE_NOT_DELETED_MSG As String = """{0}"" を" & vbCrLf & "削除できませんでした。"
+    Private Const FILE_NOT_CREATED_MSG As String = """{0}"" を" & vbCrLf & "作成できませんでした。"
+
+    ' From関連メッセージ
+    Private ReadOnly FROM_PATH_IS_EMPTY_MSG As String = String.Format(PATH_IS_EMPTY, "From")
+
+    ' To関連メッセージ
+    Private ReadOnly TO_PATH_IS_EMPTY_MSG As String = String.Format(PATH_IS_EMPTY, "To")
+    Private Const FROM_TO_DUPLICATION_MSG As String = "FromとToは別のフォルダを指定して下さい。"
+
+    ' Log関連メッセージ
+    Private Const LOG_FILE_FORMAT As String = """diffandsync_""yyyyMMddHHmmss"".log"""
+    Private ReadOnly LOG_PATH_IS_EMPTY_MSG As String = String.Format(PATH_IS_EMPTY, "Log")
+    Private Const LOG_FILE_DELETE_CONFIRM_TITLE As String = "ログファイル削除確認"
+    Private Const LOG_FILE_DELETE_CONFIRM_MSG As String = "{0}は" & vbCrLf & "すでに存在します。上書きしてもいいですか？"
+    Private Const LOG_DIRECTORY_NOT_EXISTS_MSG As String = "ログファイル保存ディレクトリ" & vbCrLf & "{0}が" & vbCrLf & "存在しません。"
+    Private Const LOG_FILE_NOT_CREATED_MSG As String = "ログファイルを作成できません。"
 
 #End Region
 
 #Region "イベント"
 
     Private Sub FromButton_Click(sender As Object, e As EventArgs) Handles FromButton.Click
-        ChooseFolder(FromBox)
+        SetChoosenFolder(FromBox)
     End Sub
 
     Private Sub ToButton_Click(sender As Object, e As EventArgs) Handles ToButton.Click
-        ChooseFolder(ToBox)
+        SetChoosenFolder(ToBox)
     End Sub
 
     Private Sub LogFolderButton_Click(sender As Object, e As EventArgs) Handles LogFolderButton.Click
-        If ChooseFolder(LogBox) Then
-            ' フォルダを選択した場合
-            ' パスにファイル名を追加
-            LogBox.Text = Path.Combine(LogBox.Text, DateTime.Now.ToString(LOG_FILE_FORMAT))
+        If Not SetChoosenFolder(LogBox) Then
+            Exit Sub
         End If
+        ' 選択したフォルダパスにファイル名を追加
+        LogBox.Text = Path.Combine(LogBox.Text, DateTime.Now.ToString(LOG_FILE_FORMAT))
     End Sub
 
     Private Sub DiffButton_Click(sender As Object, e As EventArgs) Handles DiffButton.Click
-
+        ' 実行前確認
+        If MsgBox(DIFF_CONFIRM_MSG, MsgBoxStyle.OkCancel, DIFF_CONFIRM_TITLE) <> MsgBoxResult.Ok Then
+            Exit Sub
+        End If
+        ' 実行前チェック
+        If Not CheckBefore() Then
+            Exit Sub
+        End If
     End Sub
 
     Private Sub SyncButton_Click(sender As Object, e As EventArgs) Handles SyncButton.Click
-
+        ' 実行前確認
+        If MsgBox(SYNC_CONFIRM_MSG, MsgBoxStyle.OkCancel, SYNC_CONFIRM_TITLE) <> MsgBoxResult.Ok Then
+            Exit Sub
+        End If
+        ' 実行前チェック
+        If Not CheckBefore() Then
+            Exit Sub
+        End If
     End Sub
 
 #End Region
 
-    ' 指定したフォルダやファイルをチェックする
-    Private Sub CheckFolders()
-        ' Fromのチェック
-        ' Toのチェック
-        ' Toのファイルチェック
-        ' ファイルが既にある場合は上書きするかどうか確認
-        ' 上書きとされた場合はファイルを削除
-    End Sub
+    Private Function CheckBefore() As Boolean
+        ' フォルダ・ファイルのチェック
+        Dim errorMsgs As String() = CheckFoldersAndFile()
+        If Not IsNothing(errorMsgs) Then
+            ' エラーあり
+            MsgBox(String.Join(vbCrLf & vbCrLf, errorMsgs), MsgBoxStyle.OkOnly, CHECK_ERROR_TITLE)
+            Return False
+        End If
 
+        ' ログファイル初期化
+        Dim errorMsg As String = CheckLogFile()
+        If Not IsNothing(errorMsg) Then
+            ' エラーあり
+            MsgBox(errorMsg, MsgBoxStyle.OkOnly, CHECK_ERROR_TITLE)
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    ' 指定したフォルダをチェックする
+    Private Function CheckFoldersAndFile() As String()
+        ' Fromのチェック
+        Dim fromErrorMsg As String = FolderCheck(FromBox.Text, FROM_PATH_IS_EMPTY_MSG, IS_NOT_FOLDER_MSG, IS_NOT_EXISTS_MSG)
+        ' Toのチェック
+        Dim toErrorMsg As String = FolderCheck(ToBox.Text, TO_PATH_IS_EMPTY_MSG, IS_NOT_FOLDER_MSG, IS_NOT_EXISTS_MSG)
+        ' FromTo重複チェック
+        Dim fromToErrorMsg As String = IIf(FromBox.Text = ToBox.Text, FROM_TO_DUPLICATION_MSG, Nothing)
+        ' Logのチェック
+        Dim logErrorMsg As String = FileCheck(LogBox.Text, LOG_PATH_IS_EMPTY_MSG, IS_NOT_FILE_MSG)
+
+        ' エラーメッセージをまとめる
+        Dim errorMsgs As New List(Of String)()
+        If Not IsNothing(fromErrorMsg) Then
+            errorMsgs.Add(fromErrorMsg)
+        End If
+        If Not IsNothing(toErrorMsg) Then
+            errorMsgs.Add(toErrorMsg)
+        End If
+        If Not IsNothing(fromToErrorMsg) Then
+            errorMsgs.Add(fromToErrorMsg)
+        End If
+        If Not IsNothing(logErrorMsg) Then
+            errorMsgs.Add(logErrorMsg)
+        End If
+
+        ' エラーメッセージがない場合
+        If errorMsgs.Count < 1 Then
+            Return Nothing
+        End If
+
+        ' エラーメッセージを配列で返す
+        Return errorMsgs.ToArray()
+    End Function
+
+    ' 指定したファイルをチェックする
+    Private Function CheckLogFile() As String
+        ' Logのファイルチェック
+        Dim errorMsg As String = NewFileCheck(LogBox.Text, _
+                                                 LOG_PATH_IS_EMPTY_MSG, _
+                                                 IS_NOT_FILE_MSG, _
+                                                 LOG_DIRECTORY_NOT_EXISTS_MSG, _
+                                                 LOG_FILE_DELETE_CONFIRM_TITLE, _
+                                                 LOG_FILE_DELETE_CONFIRM_MSG, _
+                                                 LOG_FILE_NOT_CREATED_MSG)
+        If Not IsNothing(errorMsg) Then
+            Return errorMsg
+        End If
+        ' ログファイルを新規作成
+        errorMsg = CreateNewEmptyFile(LogBox.Text, FILE_NOT_DELETED_MSG, FILE_NOT_CREATED_MSG)
+        If Not IsNothing(errorMsg) Then
+            Return errorMsg
+        End If
+        Return Nothing
+    End Function
 
 End Class
--- DiffAndSync/Utils.vb
+++ DiffAndSync/Utils.vb
@@ -1,31 +1,132 @@
-﻿Module Utils
+﻿Imports System.IO
+
+Module Utils
 
     ' フォルダ選択ダイアログを表示
     ' 選択したらテキストボックスにその値をセットする
-    Public Function ChooseFolder(ByRef ResultTextBox As TextBox) As Boolean
-        Dim Fbd As New FolderBrowserDialog()
-
+    Public Function ChooseFolder(ByVal defaultPath As String) As String
+        Dim fbd As New FolderBrowserDialog()
         ' 初期フォルダはデスクトップ
-        Fbd.RootFolder = System.Environment.SpecialFolder.Desktop
-
+        fbd.RootFolder = System.Environment.SpecialFolder.Desktop
         ' 初期選択するパスを設定する
-        Fbd.SelectedPath = ResultTextBox.Text
-
-        ' [新しいフォルダ] ボタンを表示する
+        fbd.SelectedPath = defaultPath
+        ' 新しいフォルダボタンを表示する
         'FolderBrowserDialog1.ShowNewFolderButton = True
+        ' ダイアログを表示
+        Try
+            If fbd.ShowDialog() = DialogResult.OK Then
+                Return fbd.SelectedPath
+            End If
+        Finally
+            ' ダイアログオブジェクトを破棄
+            fbd.Dispose()
+        End Try
+        Return Nothing
+    End Function
 
-        Dim IsChoosen As Boolean = False
-
-        ' ダイアログを表示し、戻り値が [OK] の場合は、選択したディレクトリを表示する
-        If Fbd.ShowDialog() = DialogResult.OK Then
-            IsChoosen = True
-            ResultTextBox.Text = Fbd.SelectedPath
+    ' フォルダ選択ダイアログを表示して結果をコントロールにセットする
+    Public Function SetChoosenFolder(ByRef cntrl As Control) As Boolean
+        Dim choosenFolder As String = ChooseFolder(cntrl.Text)
+        If IsNothing(choosenFolder) Then
+            ' 選択されなかったのでセットしない
+            Return False
         End If
+        cntrl.Text = choosenFolder
+        Return True
+    End Function
 
-        ' 不要になった時点で破棄する
-        Fbd.Dispose()
+    ' フォルダの存在チェックをしてメッセージを返す
+    Public Function FolderCheck(ByVal folderPath As String, ByVal msgPathIsEmpty As String, ByVal msgFormatIsNotFolder As String, ByVal msgFormatNotExists As String) As String
+        ' パスが空
+        If String.IsNullOrEmpty(folderPath) Then
+            Return String.Format(msgPathIsEmpty, folderPath)
+        End If
+        ' パスがフォルダではなくファイル
+        If File.Exists(folderPath) Then
+            Return String.Format(msgFormatIsNotFolder, folderPath)
+        End If
+        ' フォルダが存在しない
+        If Not Directory.Exists(folderPath) Then
+            Return String.Format(msgFormatNotExists, folderPath)
+        End If
+        Return Nothing
+    End Function
 
-        Return IsChoosen
+    ' ファイルの存在チェックをしてメッセージを返す
+    ' ファイルの存在チェックをしない
+    Public Function FileCheck(ByVal filePath As String, ByVal msgPathIsEmpty As String, ByVal msgFormatIsNotFile As String) As String
+        Return FileCheck(filePath, msgPathIsEmpty, msgFormatIsNotFile, Nothing)
+    End Function
+
+    ' ファイルの存在チェックをしてメッセージを返す
+    Public Function FileCheck(ByVal filePath As String, ByVal msgPathIsEmpty As String, ByVal msgFormatIsNotFile As String, ByVal msgFormatNotExists As String) As String
+        ' パスが空
+        If String.IsNullOrEmpty(filePath) Then
+            Return String.Format(msgPathIsEmpty, filePath)
+        End If
+        ' パスがファイルではなくフォルダ
+        If Directory.Exists(filePath) Then
+            Return String.Format(msgFormatIsNotFile, filePath)
+        End If
+        ' ファイルが存在しない(フォーマットがnothingの場合はチェックしない)
+        If Not IsNothing(msgFormatNotExists) AndAlso Not Directory.Exists(filePath) Then
+            Return String.Format(msgFormatNotExists, filePath)
+        End If
+        Return Nothing
+    End Function
+
+    ' 新規ファイル作成前ファイルチェック
+    ' 既にファイルが有る場合は削除するかどうか表示する
+    Public Function NewFileCheck(ByVal filePath As String, _
+                                 ByVal msgPathIsEmpty As String, _
+                                 ByVal isNotFileMsg As String, _
+                                 ByVal logDirectoryNotExistsMsg As String, _
+                                 ByVal logFileDeleteConfirmTitle As String, _
+                                 ByVal logFileDeleteConfirmMsg As String, _
+                                 ByVal logFileDeleteCanceledMsg As String) As String
+        ' パスがフォルダを指してないかチェック
+        Dim errorMsg As String = FileCheck(filePath, msgPathIsEmpty, isNotFileMsg)
+        If Not IsNothing(errorMsg) Then
+            Return errorMsg
+        End If
+        ' 親ディレクトリがあるか？
+        Dim logFolderPath As String = Path.GetDirectoryName(filePath)
+        If Not Directory.Exists(logFolderPath) Then
+            ' 親ディレクトリがない場合
+            Return String.Format(logDirectoryNotExistsMsg, logFolderPath)
+        End If
+        ' ファイルすでにあるか？
+        If Not File.Exists(filePath) Then
+            ' 正常終了
+            Return Nothing
+        End If
+        ' ファイルが既にある場合は削除するかどうか確認
+        If MsgBox(String.Format(logFileDeleteConfirmMsg, logFolderPath), MsgBoxStyle.OkCancel, logFileDeleteConfirmTitle) <> MsgBoxResult.Ok Then
+            ' 拒否の場合はエラー
+            Return String.Format(logFileDeleteCanceledMsg, filePath)
+        End If
+        Return Nothing
+    End Function
+
+    ' 空のファイル作成
+    ' 既にファイルが有る場合はゴミ箱に移動する
+    Public Function CreateNewEmptyFile(ByVal filePath As String, ByVal logFileNotDeletedMsg As String, ByVal logFileNotCreatedMsg As String) As String
+        ' 既存のファイルをゴミ箱に移動
+        If File.Exists(filePath) Then
+            Try
+                My.Computer.FileSystem.DeleteFile(filePath, FileIO.UIOption.OnlyErrorDialogs, FileIO.RecycleOption.SendToRecycleBin)
+            Catch ex As Exception
+                ' ファイル削除失敗
+                Return String.Format(logFileNotDeletedMsg, filePath)
+            End Try
+        End If
+        ' 空のログファイルを作成
+        Try
+            File.Create(filePath).Close()
+        Catch ex As Exception
+            Return String.Format(logFileNotCreatedMsg, filePath)
+        End Try
+        Return Nothing
     End Function
 
 End Module
