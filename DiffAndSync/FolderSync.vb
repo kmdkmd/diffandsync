225a496ddae95a8b0b0a5b2de9b200dfc6a3d5ed
kmdkmd <kmd217@gmail.com>
2013-11-17 18:31:56 +0900

    Toフォルダのみのファイル削除完了

--- DiffAndSync/FolderSync.vb
+++ DiffAndSync/FolderSync.vb
@@ -33,6 +33,11 @@ Public Class FolderSync
     ' 削除失敗
     Private Const NOT_DELETED_MSG As String = "{0} : 削除失敗"
 
+    ' 削除完了ファイル数
+    Private Const DELETED_FILE_NUM_MSG As String = "削除完了ファイル数 : {0}"
+    ' 削除失敗ファイル数
+    Private Const NOT_DELETED_FILE_NUM_MSG As String = "削除失敗ファイル数 : {0}"
+
     ' 削除完了フォルダ数
     Private Const DELETED_FOLDER_NUM_MSG As String = "削除完了フォルダ数 : {0}"
     ' 削除失敗フォルダ数
@@ -91,12 +96,17 @@ Public Class FolderSync
             If Not IsNothing(logWriter) Then
                 Try
                     logWriter.WriteLine("同期終了")
+
                     logWriter.WriteLine(CREATED_FOLDER_NUM_MSG, createdFolderNum)
                     logWriter.WriteLine(NOT_CREATED_FOLDER_NUM_MSG, createErrorFolderNum)
                     logWriter.WriteLine(COPIED_ATTR_FOLDER_NUM_MSG, copiedFolderAttrNum)
                     logWriter.WriteLine(NOT_COPIED_ATTR_FOLDER_NUM_MSG, copyErrorFolderAttrNum)
                     logWriter.WriteLine(DELETED_FOLDER_NUM_MSG, deletedFolderNum)
                     logWriter.WriteLine(NOT_DELETED_FOLDER_NUM_MSG, deleteErrorFolderNum)
+
+                    logWriter.WriteLine(DELETED_FILE_NUM_MSG, deletedFileNum)
+                    logWriter.WriteLine(NOT_DELETED_FILE_NUM_MSG, deleteErrorFileNum)
+
                     logWriter.Close()
                 Catch ex As Exception
                     MsgBox(ex.Message & vbCrLf & ex.StackTrace, MsgBoxStyle.OkOnly)
@@ -105,8 +115,81 @@ Public Class FolderSync
         End Try
     End Sub
 
+    ' 同期処理
     Private Sub Sync(ByVal fromFolder As String, ByVal toFolder As String)
         ' Toフォルダがなければ作成
+        CreateFolder(toFolder)
+
+        ' ファイルの同期
+        SyncFiles(fromFolder, toFolder)
+
+        ' サブフォルダの同期
+        SyncSubFolders(fromFolder, toFolder)
+
+        ' フォルダの属性コピー
+        CopyFolderAttributes(fromFolder, toFolder)
+    End Sub
+
+    ' ファイルの同期
+    Private Sub SyncFiles(ByVal fromFolder As String, ByVal toFolder As String)
+        ' Toフォルダのファイル一覧取得
+        Dim toFiles As List(Of String) = New List(Of String)(Directory.GetFiles(toFolder))
+
+        ' From-Toのループ
+        For Each fromFile As String In Directory.GetFiles(fromFolder)
+            ' Fromのファイル名取得
+            ' Fromのフォルダ名取得
+            Dim toFile As String = Path.Combine(toFolder, Path.GetFileName(fromFile))
+
+            ' Fromのファイル名と同じ名前のToのファイルがあるかチェック
+
+            ' Fromのファイルの作成日時取得
+            ' Toのファイルの作成日時取得
+            ' 作成日時比較
+
+            ' Fromのファイルの更新日時取得
+            ' Toのファイルの更新日時取得
+            ' 更新日時比較
+
+            ' Fromのファイルサイズ取得
+            ' Toのファイルサイズ取得
+            ' ファイルサイズ比較
+
+            ' FromのファイルのMD5チェックサムを取得
+            ' ToのファイルのMD5チェックサムを取得
+            ' チェックサム比較
+
+            ' ファイルをコピー
+            ' 作成日時をセット
+            ' アクセス日時をセット
+
+            ' ファイルサイズを比較
+            ' FromのファイルのMD5チェックサムを未取得の場合は取得
+            ' ToのファイルのMD5チェックサムを未取得の場合は取得
+            ' チェックサム比較
+
+            ' 同期完了したのでToのフォルダリストから削除
+            Dim deleteIndex As Integer = toFiles.IndexOf(toFile)
+            If deleteIndex >= 0 Then
+                toFiles.RemoveAt(deleteIndex)
+            End If
+        Next
+
+        ' Toディレクトリファイル一覧から削除
+        For Each toFile As String In toFiles
+            Try
+                My.Computer.FileSystem.DeleteFile(toFile, FileIO.UIOption.OnlyErrorDialogs, FileIO.RecycleOption.SendToRecycleBin)
+                logWriter.WriteLine(DELETED_MSG, toFile)
+                deletedFileNum += 1
+            Catch ex As Exception
+                logWriter.WriteLine(NOT_DELETED_MSG, toFile)
+                deleteErrorFileNum += 1
+            End Try
+        Next
+    End Sub
+
+    ' Toフォルダの作成
+    Private Sub CreateFolder(ByVal toFolder As String)
         If Not Directory.Exists(toFolder) Then
             Try
                 Directory.CreateDirectory(toFolder)
@@ -118,57 +201,6 @@ Public Class FolderSync
                 Exit Sub
             End Try
         End If
-
-
-
-        ' Fromフォルダのファイル一覧取得
-        ' Toフォルダのファイル一覧取得
-
-
-        ' From-Toのループ
-
-        ' Fromのファイル名取得
-        ' Fromのファイル名と同じ名前のToのファイルがあるかチェック
-
-        ' Fromのファイルの作成日時取得
-        ' Toのファイルの作成日時取得
-        ' 作成日時比較
-
-        ' Fromのファイルの更新日時取得
-        ' Toのファイルの更新日時取得
-        ' 更新日時比較
-
-        ' Fromのファイルサイズ取得
-        ' Toのファイルサイズ取得
-        ' ファイルサイズ比較
-
-        ' FromのファイルのMD5チェックサムを取得
-        ' ToのファイルのMD5チェックサムを取得
-        ' チェックサム比較
-
-        ' ファイルをコピー
-        ' 作成日時をセット
-        ' アクセス日時をセット
-
-        ' ファイルサイズを比較
-        ' FromのファイルのMD5チェックサムを未取得の場合は取得
-        ' ToのファイルのMD5チェックサムを未取得の場合は取得
-        ' チェックサム比較
-
-        ' コピー結果をログに書き出す
-
-        ' Toディレクトリファイル一覧から削除
-
-        ' To削除のループ
-
-        ' Toのファイル一覧でループ開始
-        ' ファイル削除
-
-        ' サブフォルダの同期
-        SyncSubFolders(fromFolder, toFolder)
-
-        ' フォルダの属性コピー
-        CopyFolderAttributes(fromFolder, toFolder)
     End Sub
 
     ' フォルダ属性、日付をコピー
@@ -212,7 +244,6 @@ Public Class FolderSync
                 logWriter.WriteLine(DELETED_MSG, toSubFolder)
                 deletedFolderNum += 1
             Catch ex As Exception
-                ' フォルダ削除失敗
                 logWriter.WriteLine(NOT_DELETED_MSG, toSubFolder)
                 deleteErrorFolderNum += 1
             End Try
